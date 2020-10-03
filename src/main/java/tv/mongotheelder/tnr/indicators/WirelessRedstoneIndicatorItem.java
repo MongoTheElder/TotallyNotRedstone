8ae7c6bc48cea824e8a533b8acbce1880947e6e4
MongoTheElder <mongotheelder@gmail.com>
2020-10-03 14:33:36 -0400

    Update WirelessRedstoneIndicatorItem to use the abstract base item class instead of the WirelessRedstoneReceiverItem class.

--- src/main/java/tv/mongotheelder/tnr/indicators/WirelessRedstoneIndicatorItem.java
+++ src/main/java/tv/mongotheelder/tnr/indicators/WirelessRedstoneIndicatorItem.java
@@ -5,10 +5,10 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import tv.mongotheelder.tnr.receivers.WirelessRedstoneReceiverItem;
 import tv.mongotheelder.tnr.receivers.WirelessRedstoneReceiverTile;
+import tv.mongotheelder.tnr.wireless.AbstractWirelessRedstoneItem;
 
-public class WirelessRedstoneIndicatorItem extends WirelessRedstoneReceiverItem {
+public class WirelessRedstoneIndicatorItem extends AbstractWirelessRedstoneItem {
     private static final Logger LOGGER = LogManager.getLogger();
 
     public WirelessRedstoneIndicatorItem(Block blockIn, Properties builder) {

32bbe65c46e3ab675b7d22a057f742f95bad489c
MongoTheElder <mongotheelder@gmail.com>
2020-10-03 16:45:15 -0400

    Removed the item/block 'ENABLE' feature from the config

--- src/main/java/tv/mongotheelder/tnr/setup/Config.java
+++ src/main/java/tv/mongotheelder/tnr/setup/Config.java
@@ -3,22 +3,19 @@ package tv.mongotheelder.tnr.setup;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 public class Config {
-    public static final String CATEGORY_SEQUENCER = "redstone_sequencer";
     public static final String CATEGORY_BUTTON = "timed_buttons";
     public static final String CATEGORY_KEYPAD = "keypad";
     public static final String CATEGORY_WIRELESS_REDSTONE_RECEIVER = "wireless_redstone_receiver";
     public static final String CATEGORY_WIRELESS_REDSTONE_INDICATOR = "wireless_redstone_indicator";
-    public static final String CATEGORY_UTILITY = "utility_items";
 
     public static ForgeConfigSpec COMMON_CONFIG;
 
-    public static ForgeConfigSpec.BooleanValue ENABLE_SEQUENCER_BLOCKS;
     public static ForgeConfigSpec.IntValue WIRELESS_REDSTONE_RECEIVER_TICK;
     public static ForgeConfigSpec.BooleanValue WIRELESS_REDSTONE_RECEIVER_UNBREAKABLE;
+
     public static ForgeConfigSpec.IntValue WIRELESS_REDSTONE_INDICATOR_TICK;
     public static ForgeConfigSpec.BooleanValue WIRELESS_REDSTONE_INDICATOR_UNBREAKABLE;
 
-    public static ForgeConfigSpec.BooleanValue ENABLE_TIMED_BUTTONS;
     public static ForgeConfigSpec.BooleanValue TIMED_BUTTONS_UNBREAKABLE;
     public static ForgeConfigSpec.IntValue TIMED_BUTTONS_PULSE_COUNT;
     public static ForgeConfigSpec.IntValue TIMED_BUTTONS_PULSE_DURATION;
@@ -26,8 +23,6 @@ public class Config {
     public static ForgeConfigSpec.BooleanValue TIMED_BUTTONS_SETTABLE_WITH_SHIFT_CLICK;
     public static ForgeConfigSpec.BooleanValue TIMED_BUTTONS_ENABLE_SOUND;
 
-    public static ForgeConfigSpec.BooleanValue ENABLE_KEYPAD;
-    public static ForgeConfigSpec.BooleanValue ENABLE_PROGRAMMER;
     public static ForgeConfigSpec.BooleanValue KEYPAD_UNBREAKABLE;
     public static ForgeConfigSpec.BooleanValue KEYPAD_SHOWS_CODE_WHEN_UNLOCKED;
 
@@ -42,11 +37,7 @@ public class Config {
         COMMON_BUILDER.comment("Totally Not Redstone");
         COMMON_BUILDER.comment("(OK, it's totally redstone)");
 
-        COMMON_BUILDER.push(CATEGORY_SEQUENCER);
-        ENABLE_SEQUENCER_BLOCKS = COMMON_BUILDER.comment(" Enable Redstone Sequencer").define("enable_redstone_sequencer", true);
-
-        COMMON_BUILDER.pop().push(CATEGORY_BUTTON);
-        ENABLE_TIMED_BUTTONS = COMMON_BUILDER.comment(" Enable Timed Buttons").define("enable_timed_buttons", true);
+        COMMON_BUILDER.push(CATEGORY_BUTTON);
         TIMED_BUTTONS_UNBREAKABLE = COMMON_BUILDER.comment(" Make timed buttons unbreakable").define("timed_buttons_unbreakable", false);
         TIMED_BUTTONS_PULSE_DURATION = COMMON_BUILDER.comment("Number of ticks per pulse").defineInRange("timed_buttons_pulse_duration", 5, 2, 5000);
         TIMED_BUTTONS_PULSE_COUNT = COMMON_BUILDER.comment(" Default number of pulses between off and on").defineInRange("timed_buttons_pulse_count_default", 5, 1, 50);
@@ -55,7 +46,6 @@ public class Config {
         TIMED_BUTTONS_ENABLE_SOUND = COMMON_BUILDER.comment(" Default setting for 'Enable Sound'").define("timed_buttons_sound", true);
 
         COMMON_BUILDER.pop().push(CATEGORY_KEYPAD);
-        ENABLE_KEYPAD = COMMON_BUILDER.comment(" Enable Keypad").define("enable_keypad", true);
         KEYPAD_UNBREAKABLE = COMMON_BUILDER.comment(" Make keypads unbreakable").define("keypad_unbreakable", false);
         KEYPAD_SHOWS_CODE_WHEN_UNLOCKED = COMMON_BUILDER.comment(" Keypad remembers correctly entered code").define("keypad_remembers_code", true);
 
@@ -67,10 +57,6 @@ public class Config {
         WIRELESS_REDSTONE_INDICATOR_TICK = COMMON_BUILDER.comment(" Number of game ticks between redstone indicator updates").defineInRange("wireless_redstone_indicator_tick", 4, 1, 20);
         WIRELESS_REDSTONE_INDICATOR_UNBREAKABLE = COMMON_BUILDER.comment(" Make wireless redstone indicators unbreakable").define("redstone_indicators_unbreakable", false);
 
-        COMMON_BUILDER.pop().push(CATEGORY_UTILITY);
-        ENABLE_PROGRAMMER = COMMON_BUILDER.comment(" Enable Programmer").define("enable_programmer", true);
-
         COMMON_BUILDER.pop();
     }
-
 }
